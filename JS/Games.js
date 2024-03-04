cff79326be461b74576f20dffa26a928a12ab3e2
Jallbee <158083923+Jallbee@users.noreply.github.com>
2024-03-04 11:45:32 -0500

    xml fix 1

--- JS/Games.js
+++ JS/Games.js
@@ -1,60 +1,47 @@
- 
-        
-        function loadReviews() {
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    displayReviews(this);
-                }
-            };
-            xhttp.open("GET", "game_reviews.xml", true);
-            xhttp.send();
+ function loadReviews() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            displayReviews(this);
         }
+    };
+    xhttp.open("GET", "game_reviews.xml", true);
+    xhttp.send();
+}
 
-        
-        function displayReviews(xml) {
-            var xmlDoc = xml.responseXML;
-            var reviews = xmlDoc.getElementsByTagName("review");
-            var html = "";
-            for (var i = 0; i < reviews.length; i++) {
-                var title = reviews[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
-                var rating = reviews[i].getElementsByTagName("rating")[0].childNodes[0].nodeValue;
-                var author = reviews[i].getElementsByTagName("author")[0].childNodes[0].nodeValue;
-                var content = reviews[i].getElementsByTagName("content")[0].childNodes[0].nodeValue;
-                html += "<div>";
-                html += "<h2>" + title + "</h2>";
-                html += "<p><strong>Rating:</strong> " + rating + "</p>";
-                html += "<p><strong>Author:</strong> " + author + "</p>";
-                html += "<p>" + content + "</p>";
-                html += "</div>";
-            }
-            document.getElementById("reviews").innerHTML = html;
-        }
+function displayReviews(xml) {
+    var xmlDoc = xml.responseXML;
+    var reviews = xmlDoc.getElementsByTagName("review");
+    var html = "";
+    for (var i = 0; i < reviews.length; i++) {
+        var title = reviews[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        var rating = reviews[i].getElementsByTagName("rating")[0].childNodes[0].nodeValue;
+        var author = reviews[i].getElementsByTagName("author")[0].childNodes[0].nodeValue;
+        var content = reviews[i].getElementsByTagName("content")[0].childNodes[0].nodeValue;
+        html += "<div>";
+        html += "<h2>" + title + "</h2>";
+        html += "<p><strong>Rating:</strong> " + rating + "</p>";
+        html += "<p><strong>Author:</strong> " + author + "</p>";
+        html += "<p>" + content + "</p>";
+        html += "</div>";
+    }
+    document.getElementById("reviews").innerHTML = html;
+}
 
-        window.onload = function() {
-            loadReviews();
-        };
-
-
-
-
-
-
+window.onload = function() {
+    loadReviews();
+};
 
 document.getElementById('gameForm').addEventListener('submit', function(event) {
-            event.preventDefault(); 
+    event.preventDefault(); 
 
-            
-            var gameName = document.getElementById('drinkName').value;
-            var description = document.getElementById('description').value;
+    var gameName = document.getElementById('gameName').value;
+    var description = document.getElementById('description').value;
 
-            
-            var suggestionElement = document.createElement('div');
-            suggestionElement.innerHTML = '<strong>' + gameName + '</strong>: ' + description;
+    var suggestionElement = document.createElement('div');
+    suggestionElement.innerHTML = '<strong>' + gameName + '</strong>: ' + description;
 
-            
-            document.getElementById('suggestions').appendChild(suggestionElement);
+    document.getElementById('suggestions').appendChild(suggestionElement);
 
-            
-            document.getElementById('gameForm').reset();
-        });
+    document.getElementById('gameForm').reset();
+});
