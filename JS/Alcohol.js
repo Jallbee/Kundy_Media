569d90569e21a3c27303b90e2f14cae7c39606cb
Jallbee <158083923+Jallbee@users.noreply.github.com>
2024-04-24 14:06:07 -0400

    fixes

--- JS/Alcohol.js
+++ JS/Alcohol.js
@@ -1,34 +1,32 @@
- function displayReviews(reviews) {
-    var reviewsContainer = $('#reviews');
-    reviews.forEach(function(review) {
-        var card = $('<div>').addClass('card');
+ $(document).ready(function() {
+    function displayReviews(reviews) {
+        var reviewsContainer = $('#reviews');
+        reviews.forEach(function(review) {
+            var card = $('<div>').addClass('card');
 
-        var productName = $('<h2>').text(review.productName);
-        card.append(productName);
+            var productName = $('<h2>').text(review.productName);
+            card.append(productName);
 
-        var starRating = $('<div>').addClass('star-rating');
-        for (var i = 1; i <= 5; i++) {
-            var star = $('<span>').addClass('star');
-            if (i <= review.rating) {
-                star.text('⭐'); // Display filled star if i is less than or equal to review.rating
-            } else {
-                star.text('☆'); // Display empty star otherwise
+            var starRating = $('<div>').addClass('star-rating');
+            for (var i = 1; i <= 5; i++) {
+                var star = $('<span>').addClass('star');
+                if (i <= review.rating) {
+                    star.addClass('selected');
+                }
+                starRating.append(star);
             }
-            starRating.append(star);
-        }
-        card.append(starRating);
+            card.append(starRating);
 
-        var reviewText = $('<p>').text(review.reviewText);
-        card.append(reviewText);
+            var reviewText = $('<p>').text(review.reviewText);
+            card.append(reviewText);
 
-        var videoLink = $('<a>').attr('href', review.videoLink).text('Watch Full Review');
-        card.append(videoLink);
+            var videoLink = $('<a>').attr('href', review.videoLink).text('Watch Full Review');
+            card.append(videoLink);
 
-        reviewsContainer.append(card);
-    });
-}
+            reviewsContainer.append(card);
+        });
+    }
 
-$(document).ready(function() {
     $.ajax({
         url: 'reviews.json',
         method: 'GET',
